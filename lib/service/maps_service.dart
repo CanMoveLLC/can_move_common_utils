c712f425678b17f1e6bd5805e69dd0b97de63f0d
wilfredasomani <devwilfredagyei@gmail.com>
2021-10-19 21:25:54 +0000

    improvements

--- lib/service/maps_service.dart
+++ lib/service/maps_service.dart
@@ -1,9 +1,11 @@
 import 'package:can_move_common_utils/model/constants.dart';
+import 'package:can_move_common_utils/ui/notify.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart' as gm;
 import 'package:google_maps_webservice/directions.dart';
 import 'package:location/location.dart' as loc;
@@ -27,8 +29,12 @@ class LoadRouteInformation {
 
 class MapsService {
   gm.LatLng? location;
-  final Stream<loc.LocationData> locationStream =
-      loc.Location().onLocationChanged;
+  final Stream<gm.LatLng> locationStream = Geolocator.getPositionStream(
+    distanceFilter: 200,
+    intervalDuration: Duration(seconds: 10),
+  )
+      .map((event) => gm.LatLng(event.latitude, event.longitude))
+      .handleError((error) => logError(error));
 
   Future changeLocationSettings({
     loc.LocationAccuracy? accuracy = loc.LocationAccuracy.high,
@@ -49,9 +55,41 @@ class MapsService {
 
   Future<gm.LatLng?> getUserLocation() async {
     try {
-      var l = await loc.Location().getLocation();
-      location = gm.LatLng(l.latitude!, l.longitude!);
-      return location;
+      // Test if location services are enabled.
+      bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+      if (!serviceEnabled) {
+        // Location services are not enabled don't continue
+        // accessing the position and request users of the
+        // App to enable the location services.
+        notify('Location services are disabled.');
+        return null;
+      }
+
+      LocationPermission permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied) {
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied) {
+          // Permissions are denied, next time you could try
+          // requesting permissions again (this is also where
+          // Android's shouldShowRequestPermissionRationale
+          // returned true. According to Android guidelines
+          // your App should show an explanatory UI now.
+          notify('Location permissions are denied');
+          return null;
+        }
+      }
+
+      if (permission == LocationPermission.deniedForever) {
+        // Permissions are denied forever, handle appropriately.
+        notify(
+            'Location permissions are permanently denied, we cannot request permissions.');
+        return null;
+      }
+
+      // When we reach here, permissions are granted and we can
+      // continue accessing the position of the device.
+      var position = await Geolocator.getCurrentPosition();
+      return gm.LatLng(position.latitude, position.longitude);
     } on PlatformException catch (error, stack) {
       if (error.code != "PERMISSION_DENIED") logError(error, stack);
       return null;
@@ -93,6 +131,61 @@ class MapsService {
           lat: destination.location.latitude,
           lng: destination.location.longitude,
         ),
+      );
+      if (res.isOkay &&
+          res.routes.isNotEmpty &&
+          res.routes.first.legs.isNotEmpty) {
+        var distance = 0.0;
+        for (var leg in res.routes.first.legs) {
+          distance += leg.distance.value.toDouble();
+        }
+        distance = distance / 1609; // convert meters to miles
+        return LoadRouteInformation(
+          price: distance.round() * 1.0 /* dollar */,
+          polyline: _createRoute(res.routes.first.overviewPolyline.points),
+          distance: distance,
+          bounds: gm.LatLngBounds(
+            northeast: gm.LatLng(
+              res.routes.first.bounds.northeast.lat,
+              res.routes.first.bounds.northeast.lng,
+            ),
+            southwest: gm.LatLng(
+              res.routes.first.bounds.southwest.lat,
+              res.routes.first.bounds.southwest.lng,
+            ),
+          ),
+        );
+      } else {
+        return null;
+      }
+    } on Exception catch (e, stack) {
+      logError(e, stack);
+      return null;
+    }
+  }
+
+  Future<LoadRouteInformation?> getDirection({
+    required LoadLocation origin,
+    required List<LoadLocation> destinations,
+  }) async {
+    try {
+      var directions = GoogleMapsDirections(apiKey: mapKey);
+      var res = await directions.directionsWithLocation(
+        Location(
+          lat: origin.location.latitude,
+          lng: origin.location.longitude,
+        ),
+        Location(
+          lat: destinations.last.location.latitude,
+          lng: destinations.last.location.longitude,
+        ),
+        waypoints: destinations
+            .sublist(0, destinations.length - 1)
+            .map((e) => Waypoint.fromLocation(Location(
+                  lat: e.location.latitude,
+                  lng: e.location.longitude,
+                )))
+            .toList(),
       );
       if (res.isOkay &&
           res.routes.isNotEmpty &&
